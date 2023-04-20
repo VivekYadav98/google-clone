766a3d64ebf215407444259b619f4514941a75f3
Vivek <vivekybdh123@gmail.com>
2023-04-20 21:59:50 +0530

    api error resolved

--- src/pages/useGoogleSearch.jsx
+++ src/pages/useGoogleSearch.jsx
@@ -2,15 +2,14 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import API_KEY from "../Keys";
 const CONTEXT_KEY = "b75ffc9ed913d4f79"
+import { BASE_URL } from "./url";
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
 
-    const baseurl = 'https:www.googleapis.com'
-
     useEffect(() => {
         const fetchData = async () => {
-            fetch(`${baseurl}/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`).then((response) => response.json()).then((result) => {
+            fetch(`${BASE_URL}/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`).then((response) => response.json()).then((result) => {
                 setData(result);
             })
         }
